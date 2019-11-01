9ba31d56c554d36ffb4f95d18a602ae8cc2c545e
whileD <whiled.tech@gmail.com>
2019-11-01 22:43:33 +0900

    add rofi config

---
+++ .config/rofi/system.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euCo pipefail
+
+declare -Ar menu=(
+  ["look"]="i3lock-next"
+
+  ["reboot"]="systemctl reboot"
+  ["suspend"]="systemctl suspend"
+  ["shutdown"]="systemctl shutdown"
+)
+
+function main() {
+  # 区切り文字の変更
+  local -r IFS=$'\n'
+
+  [[ $# -ne 0 ]] && eval "${menu[$1]}" || echo "${!menu[*]}"
+}
+
+main $@
